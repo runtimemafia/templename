651f0fd3f31f7d9db9236dc37cb117ec6e386c15
Yash <yashskolambekar2015@gmail.com>
2025-02-25 21:07:39 +0530

    cf worker config

---
+++ .open-next/env/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"TEMPLE_VALID_PATHS":"/, /book, /contact","TEMPLE_EXCLUDED_SUBDOMAINS":"www,staging,dev,temples"};
+export const development = {"TEMPLE_VALID_PATHS":"/, /book, /contact","TEMPLE_EXCLUDED_SUBDOMAINS":"www,staging,dev,temples"};
+export const test = {"TEMPLE_VALID_PATHS":"/, /book, /contact","TEMPLE_EXCLUDED_SUBDOMAINS":"www,staging,dev,temples"};
